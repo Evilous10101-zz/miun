298abf3963e826ecfc1c3920d3c4ff5d8130da15
Michael Lannon <sMichaelLannon@yccc.edu>
2018-10-10 17:47:45 -0400

    added some stuff

--- src/miun/Hang.java
+++ src/miun/Hang.java
@@ -3,12 +3,12 @@ package miun;
 import java.util.Scanner;
 
 public class Hang {
-	
-	private static String[] words = {"cat", "dog", "spider", "chicken", "word", "coke" };
+	//I want to try to get the game to autogenerate a word instead of choosing one of these words. 
+	private static String[] words = {"cat", "dog", "spider", "chicken", "word", "coke","hangman","game","java","window" };
 	private static String word = words[(int) (Math.random() * words.length)];
 	private static String asterisk = new String(new char[word.length()]).replace("\0", "*");
 	private static int count = 0;
-
+	//Code above generates a mask from a random word from the words string
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
@@ -18,6 +18,7 @@ public class Hang {
 			String guess = sc.next();
 			hang(guess);
 		}
+		//If there is a try left it will ask the user for input
 		sc.close();
 	}
 	
@@ -32,7 +33,7 @@ public class Hang {
 				newasterisk += "*";
 			}
 		}
-
+//Code to deal with mask above 
 		if (asterisk.equals(newasterisk)) {
 			count++;
 			hangmanWrongguesses();
@@ -43,39 +44,32 @@ public class Hang {
 			System.out.println("Correct! You win! The word was " + word);
 			System.exit(0);
 		}
+		//Gives player input that they screwed up, got a letter right or won
 	}
-	
 	public static void hangmanWrongguesses() {
 		if (count == 1) {
 			System.out.println("Wrong guess, you have 6 trys left");
-			
 		}
 		if (count == 2) {
 			System.out.println("Wrong guess, you have 5 trys left");
-			
 		}
 		if (count == 3) {
 			System.out.println("Wrong guess, you have 4 trys left");
-			
 		}
 		if (count == 4) {
 			System.out.println("Wrong guess, you have 3 trys left");
-			
 		}
 		if (count == 5) {
 			System.out.println("Wrong guess, you have 2 trys left");
-			
 		}
 		if (count == 6) {
 			System.out.println("Wrong guess, you have one try left");
-			
 		}
 		if (count == 7) {
 			System.out.println("You used all of your trys");
-			
 			System.out.println("GAME OVER! The word was " + word);
-			
 			System.exit(0); 
+			//Tells player that they got a letter wrong and then tells the user the word when they lose. It also kills the game when lost. I want to add in a way to get it to reset if the user chooses. 
 		}
 	}
 }
