c6e8e6391b7c612449df1a279cf04e727e0301ae
Michael Lannon <sMichaelLannon@yccc.edu>
2018-10-10 18:03:22 -0400

    update

--- src/miun/Hang.java
+++ src/miun/Hang.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Hang {
 		//I want to try to get the game to autogenerate a word instead of choosing one of these words. 
-	private static String[] words = {"cat", "dog", "spider", "chicken", "word", "coke","hangman","game","java","window" };
+	private static String[] words = {"cat", "dog", "spider", "chicken", "word", "coke","hangman","game","java","window", "a", "chrome", "black", "board", "information"};
 	private static String word = words[(int) (Math.random() * words.length)];
 	private static String asterisk = new String(new char[word.length()]).replace("\0", "*");
 	private static int count = 0;
@@ -12,7 +12,7 @@ public class Hang {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
-		while (count < 7 && asterisk.contains("*")) {
+		while (count < 5 && asterisk.contains("*")) {
 			System.out.println("Guess any letter in the word");
 			System.out.println(asterisk);
 			String guess = sc.next();
@@ -44,28 +44,25 @@ public class Hang {
 			System.out.println("You win! The word was " + word);
 			System.exit(0);
 		}
-			//Gives player input that they screwed up, got a letter right or won the game.
+			//Gives player input that they screwed up, got a letter right or won the game. I might have to lower the amount of tries, it just seems to easy. 7 to 5 for now.
 	}
 	public static void hangmanWrongguesses() {
+		
+		} 
+	{
 		if (count == 1) {
-			System.out.println("Wrong guess, you have 6 trys left");
-		}
-		if (count == 2) {
-			System.out.println("Wrong guess, you have 5 trys left");
-		}
-		if (count == 3) {
 			System.out.println("Wrong guess, you have 4 trys left");
 		}
-		if (count == 4) {
+		if (count == 2) {
 			System.out.println("Wrong guess, you have 3 trys left");
 		}
-		if (count == 5) {
+		if (count == 3) {
 			System.out.println("Wrong guess, you have 2 trys left");
 		}
-		if (count == 6) {
+		if (count == 4) {
 			System.out.println("Wrong guess, you have 1 try left");
 		}
-		if (count == 7) {
+		if (count == 5) {
 			System.out.println("You used all of your trys");
 			System.out.println("GAME OVER! The word was " + word);
 			System.exit(0); 
